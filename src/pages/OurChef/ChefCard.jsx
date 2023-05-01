422d0b746a7d558b653e33664bf3268ede2a3d7b
habibullahraju <raju.ra423@gmail.com>
2023-05-02 03:51:57 +0600

    i add all chef cart design and fecth data

---
+++ src/pages/OurChef/ChefCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaThumbsUp } from "react-icons/fa";
+
+const ChefCard = ({chef}) => {
+    console.log(chef);
+    const {id, chefPicture,chefName, yearsOfExperience, numberOfRecipes, likes} = chef;
+    return (
+        <div className='mx-auto'>
+            <div className="card w-100 bg-base-100 shadow-xl">
+  <figure><img src="https://img.freepik.com/premium-photo/according-recipe-man-bearded-chef-cooking-food-guy-read-book-recipes-culinary-arts-concept-man-learn-recipe-improve-cooking-skill-ultimate-cooking-guide-beginners-book-family-recipes_474717-42831.jpg?w=1060" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{chefName}</h2>
+    <p className='m-0'>Experience: {yearsOfExperience} years</p>
+    <p>Recipes: {numberOfRecipes}</p>
+    <p className='flex items-center gap-3'><span><FaThumbsUp></FaThumbsUp></span><span>{likes}</span></p>
+    <div className="card-actions justify-end">
+      <button className="btn  bg-[#C59D5F]">View Recipe</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default ChefCard;
---
+++ src/pages/OurChef/OurChef.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import ChefCard from './ChefCard';
+
+const OurChef = () => {
+    const [chefs,  setChefs] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/all-chef')
+        .then(res =>res.json())
+        .then(data => setChefs(data))
+    },[])
+    return (
+        <div>
+            <h2 className='text-5xl font-bold text-center mt-16 py-5'>Our Professional Chef</h2>
+            <p className='text-center px-5 mb-6 text-gray-500 md:px-40'> Led by our executive chef, each member of our kitchen staff is passionate about using the freshest ingredients and incorporating bold flavors and innovative techniques to elevate classic recipes.</p>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-10 px-10 '>
+                {
+                    chefs.map(chef => <ChefCard
+                        chef={chef}
+                        key={chef.id}
+                    ></ChefCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default OurChef;
